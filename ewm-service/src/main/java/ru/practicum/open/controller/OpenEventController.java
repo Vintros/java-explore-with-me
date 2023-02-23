e9f7b71f7ee1e947e7f146eeab3be9ab40752831
Tiutin Viacheslav <tyutin-vy@yandex.ru>
2023-02-23 13:47:21 +0300

    refactor: change validation

--- ewm-service/src/main/java/ru/practicum/open/controller/OpenEventController.java
+++ ewm-service/src/main/java/ru/practicum/open/controller/OpenEventController.java
@@ -2,6 +2,7 @@ package ru.practicum.open.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.common.dto.EventFullDto;
 import ru.practicum.common.dto.EventShortDto;
@@ -11,7 +12,6 @@ import util.SortParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -23,25 +23,19 @@ public class OpenEventController {
     private final OpenEventService service;
 
     @GetMapping
-    public List<EventShortDto> getEvents(@RequestParam(required = false) String text,
-                                         @RequestParam(required = false) List<Long> categories,
-                                         @RequestParam(required = false) Boolean paid,
-                                         @RequestParam(required = false) String rangeStart,
-                                         @RequestParam(required = false) String rangeEnd,
-                                         @RequestParam(defaultValue = "false") Boolean onlyAvailable,
-                                         @RequestParam(required = false) SortParam sort,
-                                         @RequestParam(defaultValue = "0") Integer from,
-                                         @RequestParam(defaultValue = "10") Integer size,
-                                         HttpServletRequest request) {
-        LocalDateTime start = null;
-        LocalDateTime end = null;
-        if (rangeStart != null) {
-            start = LocalDateTime.parse(rangeStart, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        }
-        if (rangeEnd != null) {
-            end = LocalDateTime.parse(rangeEnd, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        }
-        EventRequestParams params = new EventRequestParams(text, categories, paid, start, end, onlyAvailable, sort, from, size);
+    public List<EventShortDto> getEvents(
+            @RequestParam(required = false) String text,
+            @RequestParam(required = false) List<Long> categories,
+            @RequestParam(required = false) Boolean paid,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeStart,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeEnd,
+            @RequestParam(defaultValue = "false") Boolean onlyAvailable,
+            @RequestParam(required = false) SortParam sort,
+            @RequestParam(defaultValue = "0") Integer from,
+            @RequestParam(defaultValue = "10") Integer size,
+            HttpServletRequest request) {
+        EventRequestParams params = new EventRequestParams(text, categories, paid, rangeStart, rangeEnd,
+                onlyAvailable, sort, from, size);
         log.info("Get events by params: {}", params);
         return service.getEvents(params, request);
     }
